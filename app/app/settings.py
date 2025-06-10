39f17f676ce0b0e000bf95b82e3b72bc88275390
matei1308 <leomatamoros7@yahoo.es>
2025-06-10 11:22:28 +0200

    Cofiguraciones para la BD

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')            
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='algun-valor-secreto-temporal-para-build')            
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == "true"
